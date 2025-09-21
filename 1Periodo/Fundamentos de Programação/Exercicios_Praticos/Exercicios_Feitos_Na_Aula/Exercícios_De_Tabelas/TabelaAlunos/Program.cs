ec43e8838ee1d1fcc48cb0b9a384026162d39b04
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-21 19:33:42 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercicios_Feitos_Na_Aula/Exercícios_De_Tabelas/TabelaAlunos/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(){
+        string nome1= "Ana";
+        string nome2="Bruno";
+        string nome3="Carços";
+        string nome4="Daniel";
+        string nome5="Eva";
+        string nome6="Ivo";
+    }
+}
