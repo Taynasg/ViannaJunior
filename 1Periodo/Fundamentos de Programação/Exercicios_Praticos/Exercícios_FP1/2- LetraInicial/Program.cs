45cd896d160d0b248616e08faa8f986ec6c299c7
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 08:23:38 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercícios_FP1/2- LetraInicial/Program.cs
@@ -0,0 +1,14 @@
+﻿//Sabendo que a instrução Console.WriteLine("") imprime o 
+//texto que estiver entre aspas e salta a linha, faça um 
+//programa que escreva a inicial de seu nome utilizando 
+//sequências de espaços e letras X. 
+
+class program{
+      static void Main(string[] args){
+        Console.WriteLine("XXXXXXXX");
+        Console.WriteLine("XXXXXXXX");
+        Console.WriteLine("  XXX   ");
+        Console.WriteLine("  XXX   ");
+        Console.WriteLine("  XXX   ");
+      }
+}
