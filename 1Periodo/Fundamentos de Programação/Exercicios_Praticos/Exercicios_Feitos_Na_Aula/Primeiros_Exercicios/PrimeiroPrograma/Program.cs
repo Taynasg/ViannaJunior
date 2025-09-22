7e5018a5bb3fee2c75e118397deb514d5a737324
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 08:08:24 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercicios_Feitos_Na_Aula/Primeiros_Exercicios/PrimeiroPrograma/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int idade;
+   
+        Console.Write("Informe a sua idade: ");
+        idade = Convert.ToInt32(Console.ReadLine());                     
+   
+        Console.WriteLine("Em 2026 você terá {0} anos ", idade +1);
+    }
+}
+
+
