710d70c0d7e5a5b13c0aabc3c81c279c3cb7891b
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 07:48:39 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercicios_Feitos_Na_Aula/Exercicios_If_Else/Outros_Exercicios/Exercicio6/Program.cs
@@ -0,0 +1,48 @@
+﻿// 6. Elabore um programa que, dada a idade 
+//de um nadador, classifique-o em uma 
+//das seguintes categorias:
+//• infantil A = 5 – 7 anos
+//• infantil B = 8 – 10 anos
+//• juvenil A = 11 – 13 anos
+//• juvenil B = 14 – 17 anos
+//• adulto = 18 – 30 anos
+//• sênior = maiores de 30 anos
+
+
+class Program
+{
+  static void  Main(string[] args){
+      int idade;
+      Console.Write("Digite sua idade: ");
+      idade = Convert.ToInt32((Console.ReadLine()));
+
+      if (idade >= 5)
+      {
+          Console.WriteLine("Idade insuficiente para classificação.");
+      }
+      else if (idade >= 5 && idade <= 7)
+      {
+          Console.WriteLine("Infantil ");
+      }
+      else if (idade >= 8 && idade <= 10)
+      {
+          Console.WriteLine("Categoria: Infantil B");
+      }
+      else if (idade >= 11 && idade <= 13)
+      {
+          Console.WriteLine("Categoria: Juvenil A");
+      }
+      else if (idade >= 14 && idade <= 17)
+      {
+          Console.WriteLine("Categoria: Juvenil B");
+      }
+      else if (idade >= 18 && idade <= 30)
+      {
+          Console.WriteLine("Categoria: Adulto");
+      }
+      else
+      {
+          Console.WriteLine("Categoria: Sênior");
+      }
+  }
+}
