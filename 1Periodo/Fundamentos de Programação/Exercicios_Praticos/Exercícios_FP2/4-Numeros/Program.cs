2766c9daf4ba49fdc7ba61eb6cae3b903dc289d2
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 08:33:10 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercícios_FP2/4-Numeros/Program.cs
@@ -0,0 +1,17 @@
+﻿// Faça um programa que leia um número inteiro 
+//e imprima o seu antecessor e sucessor
+class Program
+{
+  static void Main(string[] args)
+  {
+    int numeroEscolhido;
+    int antecessor;
+    int sucessor;
+    Console.Write("Digite um número e eu te direi o antecessor e o sucessor dele: ");
+    numeroEscolhido = Convert.ToInt32(Console.ReadLine());
+    sucessor = numeroEscolhido + 1;
+    antecessor = numeroEscolhido - 1;
+    Console.Write("O antecessor de " + numeroEscolhido + " é " + antecessor + " e o sucessor é " + sucessor);
+  }
+}
+
---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercícios_FP2/5-Desconto/Program.cs
@@ -0,0 +1,16 @@
+﻿// Construa um programa que aplique um 
+//desconto de 25% sobre o preço de um 
+//produto recebido como entrada e imprima o 
+//valor resultante.
+class Program{
+  static void Main(){
+    double numeroDigitado;
+    double valorDescontado;
+    double valorFinal;
+    Console.WriteLine("Coloque um preço e eu aplicarei um desconto de 25% nele");
+    numeroDigitado = Convert.ToDouble(Console.ReadLine());
+    valorDescontado = 25*(numeroDigitado/100);
+    valorFinal = numeroDigitado - valorDescontado;
+    Console.Write("O valor final é {0,N2}",  valorFinal );
+  }
+}
