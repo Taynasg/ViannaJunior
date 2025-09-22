9eeb57ff49c38274c15822d14d1627062ad8991f
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 08:31:43 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercícios_FP2/2-Salario/Program.cs
@@ -0,0 +1,15 @@
+﻿// Faça um programa que lê um valor de salário 
+//mínimo e o salário de um funcionário. O programa 
+//deve calcular e imprimir quantos salários mínimos 
+//esse funcionário ganha.
+
+class Program {
+  static void Main(string[] args){
+    double salario;
+    double mediaDeSalariosMinimos; 
+    Console.Write("Digite o quanto você ganha: ");
+    salario = Convert.ToDouble(Console.ReadLine());
+    mediaDeSalariosMinimos = salario/1518;
+    Console.Write("Você ganha em torno de " + mediaDeSalariosMinimos + " salário(s) mínimo(s)");
+  }
+}
