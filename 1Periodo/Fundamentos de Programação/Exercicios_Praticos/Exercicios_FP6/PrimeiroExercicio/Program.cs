585d45f1cc346e59a120552dbbd9b608799382c5
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 22:37:22 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercicios_FP6/PrimeiroExercicio/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PrimeiroExercicio;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] idades = new int[5]
+
+        for (int i = 0; i < length; i++)
+        {
+         Console.WriteLine("Idades{0} de {1} ", i + 1,idades.length );  
+         idades[i] = Convert.ToInt32(Con) 
+      
+        }
+    }
+}
