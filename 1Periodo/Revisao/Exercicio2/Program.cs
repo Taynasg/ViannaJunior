6c6e51e4585a1b45ebf48c16594201af44d28012
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 22:43:17 -0300

    Add files via upload

---
+++ 1Periodo/Revisao/Exercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+
+  static void RetornarNumero(int numeroDigitado)
+  {
+    Console.Write(numeroDigitado);
+  }
+
+static void Main(string[] args)
+{
+  int numeroDigitado =5;
+  while(numeroDigitado != 0){
+    RetornarNumero(5);
+  }
+}
+}
