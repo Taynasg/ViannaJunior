7e24f890f5d57e1f5a270fe3f7834efe43f0fef3
Taynasg <115035153+Taynasg@users.noreply.github.com>
2025-09-22 22:36:15 -0300

    Add files via upload

---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercicios_FP5/Exercício3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ler um número inteiro e positivo e verificar se este é ou 
+//não um número primo
+class Program{
+  static void LerNumeroa(int numero)
+  {
+    
+    if (numero % 1 == 0 && numero % numero == 0)
+    {
+      Console.Write("{0} é primo", numero);
+    }
+    else
+    {
+      Console.Write("{0} não é primo", numero);
+    }
+  }
+    static void Main(string[] args)
+    {
+      int numero;
+      Console.Write("Digite um número inteiro: ");
+      numero = Convert.ToInt32(Console.ReadLine());
+      LerNumeroa(numero);
+    }
+  }
---
+++ 1Periodo/Fundamentos de Programação/Exercicios_Praticos/Exercicios_FP5/Exercício5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Escrever um algoritmo que lê um valor N inteiro e positivo e que calcula e 
+//escreve o valor de E.
+//E = 1 + 1/1! + 1/2! + 1/3! + ... + 1/N!
+
+class Program
+{
+    static int CauculaFatorial(int valorDigitado)
+    {
+        for (int fatorial = 0; valorDigitado <= 1; valorDigitado--)
+        {
+            fatorial = 1 + 1/valorDigitado;
+        }
+        return fatorial;
+    }
+    static void Main(string[] args)
+    {
+        int valorDigitado;
+      
+        Console.Write("Escreva um inteiro e positivo: ");
+        valorDigitado = Convert.ToInt32((Console.ReadLine()));
+        CauculaFatorial(valorDigitado);
+        Console.Write("O fatorial de {0} é: {1} ", valorDigitado);
+    }
+}
