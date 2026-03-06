c70f17314cc244424de63cade0ca0ccb65d6064b
Taynasg <115035153+Taynasg@users.noreply.github.com>
2026-03-05 21:58:37 -0300

    Add files via upload

---
+++ 2Periodo/TWA/tp3/script.js
@@ -0,0 +1,25 @@
+let amigos = new Array("Zezinho, Luisinho, Huguinnho ");
+let amigos1 = ["Zezinho, Luisinho, Huguinnho ", 123, true];
+
+let teste = new Array(10);
+console.log( amigos[-1] = "bia");
+console.log(amigos.length);
+console.log(amigos.lastIndexOf);
+
+let amigos2 =["Huguinho", "Zezinho", "Luisinho"];
+amigos.push("Tio Patihas");
+
+let novoAmigo = amigos.splice(1,0,"Mickey");
+console.log(amigos);
+
+delete amigos[1];
+
+console.log(amigos);
+
+// Exercício
+
+let lista = new Array();
+lista = ["Leite","Pão","Maçãs"]; 
+amigos[1].splice(1,1);
+amigos.push();
+novaLista = [amigos.push]
