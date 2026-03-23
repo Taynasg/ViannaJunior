d3114161166b062fa563521e343ffe9166d0a6c1
Taynasg <115035153+Taynasg@users.noreply.github.com>
2026-03-23 20:39:08 -0300

    Add files via upload

---
+++ 2Periodo/TWA/23-03/script.js
@@ -0,0 +1,16 @@
+let numeroDeElefantes = 1;
+let numeroDePalavra = "incomodam"
+
+
+
+console.log( numeroDeElefantes + " elefante incomoda muita gente");
+for (numeroDeElefantes =1; numeroDeElefantes < 11; numeroDeElefantes++) {
+numeroDeElefantes++
+
+while(numeroDeElefantes<10){
+console.log( numeroDeElefantes + " elefantes " + numeroDePalavra + " muito mais");
+}
+} 
+
+
+
