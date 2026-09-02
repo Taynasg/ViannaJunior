88feccf7dcfc31d88fdcf6fde01a7ac7e0bb7d2c
Taynasg <115035153+Taynasg@users.noreply.github.com>
2026-09-01 21:51:26 -0300

    Add files via upload

---
+++ 3Periodo/PFS/Curso-Next/my-app-next/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl text-blue-600 font-bold">
+        Este é um curso sobre Next.js!
+      </h1>
+      <h1 className="text-xl text-gray-500 font-bold">
+        Espero que você curta o andamento do nosso curso.
+      </h1>
+    </div>
+  );
+}
